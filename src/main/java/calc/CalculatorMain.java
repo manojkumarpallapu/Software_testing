a09a0d94479eae827b832d4e7cc87b541a387d9f
sriram <nidamanurisriram949@gmail.com>
2018-03-16 02:33:28 +0100

    upload sriram

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -6,8 +6,25 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
+	public int Sub(int a, int b) {
+		return a-b;
+	}
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		if(b==0) {
+			return 0;
+		}
+		else {
+			return a/b;
+		}
+	}
 	public Object Square(int a) {
-		// TODO Auto-generated method stub
 		return a*a;
+	}
+	public Object fact(int a) {
+		// TODO Auto-generated method stub
+		return a*(a-1);
 	}
 }
