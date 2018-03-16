69e22d42935061e3a716b3134c889c2bbebc7fbc
sriram <nidamanurisriram949@gmail.com>
2018-03-16 01:45:17 +0100

    upload div

--- src/test/java/calc/CaculatorTest.java
+++ src/test/java/calc/CaculatorTest.java
@@ -13,32 +13,34 @@ public class CaculatorTest {
 		calc = new CalculatorMain();
 	}
 	
-	//@Test
-	//public void testAdd() {
-		//assertEquals(5, calc.add(3,2));
+       @Test
+	public void testAdd() {
+		assertEquals(5, calc.add(3,2));
+       }
 	//}
 	//@Test
 	//public void testSub() {
 	//	assertEquals(1, calc.Sub(3,2));
 	//}
+	//@Test
+	//public void testMul() {
+	//assertEquals(6, calc.mul(3,2));
+	//}
 	@Test
-	public void testMul() {
-		assertEquals(6, calc.mul(3,2));
+	public void testDiv() {
+		assertEquals(1, calc.div(2,2));
 	}
-//	@Test
-//	public void testDiv() {
-//		assertEquals(1, calc.div(2,2));
-//	}
-//	@Test
-//	public void testDivByZero() {
-//		assertEquals(0, calc.div(3,0));
-//	}
-//	@Test
-//	public void testPresedence() {
-//		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
-//	}
+	@Test
+	public void testDivByZero() {
+		assertEquals(0, calc.div(3,0));
+	}
+	//@Test
+	//public void testPresedence() {
+		//assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
+		//}
 	//@Test
 	//public void testSquare() {
 		//assertEquals(4, calc.Square(2));
 	//}
 }
+
